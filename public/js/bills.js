a8620284ec0861396c29476b881310c9c7255c04
dovydaszemait <dovydas.zemas@gmail.com>
2022-03-17 22:05:05 +0200

    many 404

--- public/js/bills.js
+++ public/js/bills.js
@@ -2,6 +2,11 @@
 const BASE_URL = 'http://localhost:3000';
 
 
+const bills = await getBills();
+if (!bills.success) {
+    return;
+  }
+
 async function getBills() {
     const token = localStorage.getItem('token24');
     const resp = await fetch(`${BASE_URL}/bills/2`, {
